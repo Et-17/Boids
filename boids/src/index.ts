85a2a3fb03e926a6d41e9d62f88232d4c1710f00
Luna Kilborn <leland.kilborn@hotmail.com>
2022-08-11 18:43:09 -0400

    Add canvas and example script

--- boids/src/index.ts
+++ boids/src/index.ts
@@ -1,1 +1,10 @@
-alert("Hello, World!");
+const canvas = document.getElementById("primary-canvas") as HTMLCanvasElement;
+const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
+
+// Dynamically resize canvas element
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Draw a red rectangle
+ctx.fillStyle = '#ff0000';
+ctx.fillRect(50, 50, 200, 100);
