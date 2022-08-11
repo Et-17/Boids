62d3660d0f30d4c8b8136c960b1d98d390b74e92
Luna Kilborn <leland.kilborn@hotmail.com>
2022-08-11 17:29:24 -0400

    Create initial directory tree and configuration
    I filled out the overall directory tree and configured webpack and typescript to match. This is all packaged in a yarn project.

---
+++ boids/webpack.config.js
@@ -0,0 +1,32 @@
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require('path');
+
+const isProduction = process.env.NODE_ENV == 'production';
+
+
+const config = {
+    entry: './site/unpacked/index.js',
+    output: {
+        path: path.resolve(__dirname, 'site'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+        ],
+    },
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+
+
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
